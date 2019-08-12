8e6d7d9c9201df27b8ae42c4fc6a8ce1a5139484
EmjayAhn <emjay.data@gmail.com>
2019-08-12 12:23:18 +0900

    Feat: upload code for train on gpu

--- train.py
+++ train.py
@@ -1,15 +1,15 @@
 import torch
 
-def train_model(discriminator, generator,
+def train_model(discriminator, generator, batch_size,
                 dis_optimizer, gen_optimizer,
                 criterion, dataloader, epochs, device):
     for epoch in range(epochs):
-        for batch_images, _ in dataloader:
+        for batch_index, (batch_images, _) in enumerate(dataloader):
             # Label 생성
-            reals = torch.ones(128, 1).to(device)
-            fakes = torch.zeros(128, 1).to(device)
+            reals = torch.ones(batch_size, 1).to(device)
+            fakes = torch.zeros(batch_size, 1).to(device)
 
-            batch_images = batch_images.view(128, -1).to(device)
+            batch_images = batch_images.view(batch_size, -1).to(device)
             
             #### DISCRIMINATOR
             # Real Image 에 대한 loss
@@ -17,7 +17,7 @@ def train_model(discriminator, generator,
             dis_real_loss = criterion(dis_results, reals)
             
             # Fake Image 에 대한 loss
-            latent = torch.randn(128, 64).to(device)
+            latent = torch.randn(batch_size, 64).to(device)
             fake_images = generator(latent).detach()
             dis_results = discriminator(fake_images)
             dis_fake_loss = criterion(dis_results, fakes)
@@ -40,6 +40,12 @@ def train_model(discriminator, generator,
             generator.zero_grad()
             gen_loss.backward()
             gen_optimizer.step()
+            
+            if batch_index % 300 == 0:
+                print("EPOCH {}: BATCH: {}, discrim_loss: {}, generator_loss: {}".format(epoch, batch_index, dis_total_loss, gen_loss))
+            
+    return discriminator, generator
+
 
 
 
